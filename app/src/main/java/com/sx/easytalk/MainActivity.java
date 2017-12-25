8e87edb9c3d5a03e8ae1c2fe357cec66937d3ce7
Sunxin <sunxin0630@gmail.com>
2017-12-25 23:05:34 +0800

    添加toolbar和bottomnavigation

--- app/src/main/java/com/sx/easytalk/MainActivity.java
+++ app/src/main/java/com/sx/easytalk/MainActivity.java
@@ -1,13 +1,67 @@
 package com.sx.easytalk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.sx.easytalk.view.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.tv_title)
+    TextView mTvTitle;
+    @BindView(R.id.tool_bar)
+    Toolbar mToolBar;
+    @BindView(R.id.bottom_navigation_bar)
+    BottomNavigationBar mBottomNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mToolBar.setTitle("");
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mTvTitle.setText("消息");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuBuilder builder = (MenuBuilder) menu;
+        builder.setOptionalIconsVisible(true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_friedn:
+                showToast("添加好友");
+                // TODO: 2017/12/25 跳转添加好友
+                break;
+            case R.id.add_scan:
+                showToast("扫一扫");
+                break;
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+
+
 }
